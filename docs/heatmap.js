8f398a19f34cf09a79476788ff433d943702d233
Aayush Bajaj <aayushbajaj7@gmail.com>
2025-05-11 13:09:49 +1000

    heatmap

--- docs/heatmap.js
+++ docs/heatmap.js
@@ -10,16 +10,26 @@ canvas.height = 800;
 const COLS = canvas.width / resolution;
 const ROWS = canvas.height / resolution;
 
+class Cell {
+  constructor() {
+    this.currentstate = Math.floor(Math.random() * 2);
+    this.total = 0;
+  }
+  setState(state) {
+    this.currentstate = state;
+    this.total += state;
+  }
+}
 function buildGrid() {
   return new Array(ROWS).fill(null)
     .map(() => new Array(COLS).fill(null)
-    .map(() => Math.floor(Math.random() * 2)));
+    .map(() => new Cell()));
 }
 
 let grid = buildGrid();
 // interval like we did in snakes
 // not like we did in tetris, with manual delta times.
-const interval = 100;
+const interval = 50;
 
 setInterval(() => {
   grid = nextGen(grid);
@@ -37,10 +47,11 @@ function update() {
 */
 
 function nextGen(grid) {
-  const nextGen = grid.map(arr => [...arr]);
-  for (let row = 0; row < grid.length; row++) {
-    for (let col = 0; col < grid[row].length; col++) {
-      const cell = grid[row][col];
+  //const nextGen = grid.map(arr => [...arr]);
+  const currentGen = grid.map(arr => arr.map(cell => cell.currentstate));
+  for (let row = 0; row < currentGen.length; row++) {
+    for (let col = 0; col < currentGen[row].length; col++) {
+      let cell = currentGen[row][col];
       let numNeighbours = 0;
       // calculate the number of neighbours for each cell
       for (let i = -1; i < 2; i++) {
@@ -49,31 +60,46 @@ function nextGen(grid) {
           // not count yourself as a neighbour!
           const x_cell = (ROWS + row + i) % ROWS;
           const y_cell = (COLS + col + j) % COLS;
-          const currentNeighbour = grid[x_cell][y_cell];
+          const currentNeighbour = currentGen[x_cell][y_cell];
           numNeighbours += currentNeighbour;
           }
         }
 
       // apply rules based on numNeighbours for each cell
       if (cell === ALIVE && numNeighbours < 2) {
-        nextGen[row][col] = DEAD; // underpopulation
+        grid[row][col].setState(0); // underpopulation
       } else if (cell === ALIVE && numNeighbours > 3) { 
-        nextGen[row][col] = DEAD; // overpopulation
+        grid[row][col].setState(0); // overpopulation
       } else if (cell === DEAD && numNeighbours === 3) {
-        nextGen[row][col] = ALIVE; // reproduction 
+        grid[row][col].setState(1); // reproduction 
+      } else {
+        grid[row][col].setState(grid[row][col].currentstate);
       }
     }
   }
-  return nextGen;
+  return grid;
 }
 
 function render(grid) {
+  let maxTotal = 0;
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[row].length; col++) {
+      const cell = grid[col][row];
+      if (cell.total > maxTotal) {
+        maxTotal = cell.total
+      }
+    }
+  }
+
   for (let row = 0; row < grid.length; row++) {
     for (let col = 0; col < grid[row].length; col++) {
       const cell = grid[row][col];
       ctx.beginPath();
       ctx.rect(row * resolution, col * resolution, resolution, resolution);
-      ctx.fillStyle = cell ? 'black' : 'white';
+      //ctx.fillStyle = cell.currentstate ? 'black' : 'white';
+      const normalised = cell.total / maxTotal;
+      const h = (1.0 - normalised) * 240;
+      ctx.fillStyle = `hsl(${h}, 100%, 50%)`
       ctx.fill()
       //ctx.stroke();
     }
